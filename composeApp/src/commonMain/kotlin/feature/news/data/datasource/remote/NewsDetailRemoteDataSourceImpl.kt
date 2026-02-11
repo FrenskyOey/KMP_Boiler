130013526c6c18a47d95e4c52b0a619ff2ae21d8
FrenskyOey <frenskyoey@gmail.com>
2026-02-11 17:33:30 +0700

    implementing base model for api response

--- composeApp/src/commonMain/kotlin/feature/news/data/datasource/remote/NewsDetailRemoteDataSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/feature/news/data/datasource/remote/NewsDetailRemoteDataSourceImpl.kt
@@ -5,13 +5,20 @@ import feature.news.data.api.NewsDetailApiService
 import feature.news.data.datasource.NewsDetailDataSource
 import feature.news.data.model.response.ArticleDetailResponse
 
+import core.domain.model.AppException
+
 class NewsDetailRemoteDataSourceImpl(
     private val apiService: NewsDetailApiService
 ) : NewsDetailDataSource.Remote {
 
     override suspend fun getNewsDetail(id: Int): ArticleDetailResponse {
         return try {
-            apiService.getNewsDetail(id)
+            val response = apiService.getNewsDetail(id)
+            if (response.isSuccess) {
+                response.data
+            } else {
+                throw AppException.UnknownError("API request failed with is_success=false")
+            }
         } catch (e: Exception) {
             throw ApiErrorHandler.handleError(e)
         }
